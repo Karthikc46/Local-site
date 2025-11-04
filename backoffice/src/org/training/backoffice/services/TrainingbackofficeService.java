953371c6941b0b07967e6305f8989dc6cba1d5f8
Karthikc46 <karthikcreddy555@gmail.com>
2025-11-05 00:02:44 +0530

    Custom_folder part1

---
+++ backoffice/src/org/training/backoffice/services/TrainingbackofficeService.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2023 SAP SE or an SAP affiliate company. All rights reserved
+ */
+package org.training.backoffice.services;
+
+/**
+ * Hello World TrainingbackofficeService
+ */
+public class TrainingbackofficeService
+{
+	private static final String HELLO = "Hello";
+	public String getHello()
+	{
+		return HELLO;
+	}
+}
---
+++ gensrc/org/training/backoffice/constants/GeneratedTrainingbackofficeConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 12-May-2025, 4:27:43 pm                     ---
+ * ----------------------------------------------------------------
+ */
+package org.training.backoffice.constants;
+
+/**
+ * @deprecated since ages - use constants in Model classes instead
+ */
+@Deprecated(since = "ages", forRemoval = false)
+@SuppressWarnings({"unused","cast"})
+public class GeneratedTrainingbackofficeConstants
+{
+	public static final String EXTENSIONNAME = "trainingbackoffice";
+	
+	protected GeneratedTrainingbackofficeConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
